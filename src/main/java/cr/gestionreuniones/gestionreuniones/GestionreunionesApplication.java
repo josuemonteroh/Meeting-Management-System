0d4d3bcf8f27e4e416549d07ef6ef78a35d578f4
Josué Montero <josuedmh1104@gmail.com>
2026-02-24 19:46:11 -0600

    Initial commit - Proyecto base Spring Boot

---
+++ src/main/java/cr/gestionreuniones/gestionreuniones/GestionreunionesApplication.java
@@ -0,0 +1,13 @@
+package cr.gestionreuniones.gestionreuniones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionreunionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionreunionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/gestionreuniones/gestionreuniones/GestionreunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.gestionreuniones.gestionreuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionreunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
